70fe10fb79955017fb446f347e809b37245dde30
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2025-04-29 14:00:33 +0200

    fix: Callable in executeBlocking instead of Promise (deprecated)

--- src/main/java/pcd/ass02/dependencyAnalyserLib/impl/DependencyAnalyserLibImpl.java
+++ src/main/java/pcd/ass02/dependencyAnalyserLib/impl/DependencyAnalyserLibImpl.java
@@ -10,10 +10,10 @@ import pcd.ass02.dependencyAnalyserLib.reports.ClassDepsReport;
 import pcd.ass02.dependencyAnalyserLib.reports.PackageDepsReport;
 import pcd.ass02.dependencyAnalyserLib.reports.ProjectDepsReport;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -34,73 +34,53 @@ public class DependencyAnalyserLibImpl extends AbstractVerticle implements Depen
 
     @Override
     public Future<ClassDepsReport> getClassDependencies(String classSrcFile) {
-        return vertx.executeBlocking(promise -> {
-            try {
+        return vertx.executeBlocking(() -> {
                 String code = new String(Files.readAllBytes(Paths.get(classSrcFile)));
                 CompilationUnit cu = StaticJavaParser.parse(code);
                 List<String> dependencies = cu.findAll(ClassOrInterfaceType.class).stream()
                     .map(ClassOrInterfaceType::getNameAsString)
                     .distinct()
                     .collect(Collectors.toList());
-                ClassDepsReport report = new ClassDepsReport(classSrcFile, dependencies);
-
-                promise.complete(report);
-            } catch (Exception e) {
-                promise.fail(e);
-            }
+                return new ClassDepsReport(classSrcFile, dependencies);
         });
     }
 
     @Override
     public Future<PackageDepsReport> getPackageDependencies(String packageSrcFolder) {
-        return vertx.executeBlocking(promise -> {
-            try {
-                List<Future<ClassDepsReport>> classFutures = new ArrayList<>();
-                try (Stream<Path> stream = Files.walk(Paths.get(packageSrcFolder))) {
-                    stream.filter(path -> path.toString().endsWith(".java"))
-                            .forEach(classFile -> classFutures.add(getClassDependencies(classFile.toString())));
-                }
-                Future.all(classFutures).onComplete(allResults -> {
-                    if (allResults.succeeded()) {
-                        List<ClassDepsReport> packageDependencies = new ArrayList<>();
-                        allResults.result().list().forEach(result -> packageDependencies.add((ClassDepsReport) result));
-
-                        PackageDepsReport report = new PackageDepsReport(packageSrcFolder, packageDependencies);
-                        promise.complete(report);
-                    } else {
-                        promise.fail(allResults.cause());
-                    }
-                });
-            } catch (Exception e) {
-                promise.fail(e);
-            }
-        });
+        try (Stream<Path> stream = Files.walk(Paths.get(packageSrcFolder))) {
+            List<Future<ClassDepsReport>> classFutures = stream
+                    .filter(path -> path.toString().endsWith(".java"))
+                    .map(path -> getClassDependencies(path.toString()))
+                    .collect(Collectors.toList());
+            return Future.all(classFutures).compose(allResults -> {
+                List<ClassDepsReport> reports = allResults.result().list()
+                        .stream()
+                        .map(result -> (ClassDepsReport) result)
+                        .collect(Collectors.toList());
+                return Future.succeededFuture(new PackageDepsReport(packageSrcFolder, reports));
+            });
+        } catch (IOException e) {
+            return Future.failedFuture(e);
+        }
     }
 
     @Override
     public Future<ProjectDepsReport> getProjectDependencies(String projectSrcFolder) {
-        return vertx.executeBlocking(promise -> {
-            try {
-                List<Future<PackageDepsReport>> packageFutures = new ArrayList<>();
-                try (Stream<Path> stream = Files.walk(Paths.get(projectSrcFolder))) {
-                    stream.filter(Files::isDirectory)
-                            .forEach(packageFolder -> packageFutures.add(getPackageDependencies(packageFolder.toString())));
-                }
-                Future.all(packageFutures).onComplete(allResults -> {
-                    if (allResults.succeeded()) {
-                        List<PackageDepsReport> projectDependencies = new ArrayList<>();
-                        allResults.result().list().forEach(result -> projectDependencies.add((PackageDepsReport) result));
-
-                        ProjectDepsReport report = new ProjectDepsReport(projectDependencies);
-                        promise.complete(report);
-                    } else {
-                        promise.fail(allResults.cause());
-                    }
-                });
-            } catch (Exception e) {
-                promise.fail(e);
-            }
-        });
+        try (Stream<Path> stream = Files.walk(Paths.get(projectSrcFolder))) {
+            List<Future<PackageDepsReport>> packageFutures = stream
+                    .filter(Files::isDirectory)
+                    .map(path -> getPackageDependencies(path.toString()))
+                    .collect(Collectors.toList());
+            return Future.all(packageFutures).compose(allResults -> {
+                List<PackageDepsReport> reports = allResults.result().list()
+                        .stream()
+                        .map(result -> (PackageDepsReport) result)
+                        .collect(Collectors.toList());
+                return Future.succeededFuture(new ProjectDepsReport(reports));
+            });
+        } catch (IOException e) {
+            return Future.failedFuture(e);
+        }
     }
 
 }
