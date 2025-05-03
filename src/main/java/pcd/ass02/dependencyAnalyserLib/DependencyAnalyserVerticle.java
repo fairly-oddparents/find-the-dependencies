5ee8e256d4bd98623ce6858746f23aaa17e3bdcb
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2025-05-03 16:14:38 +0200

    add: implement DependencyAnalyserVerticle for dependency analysis

---
+++ src/main/java/pcd/ass02/dependencyAnalyserLib/DependencyAnalyserVerticle.java
@@ -0,0 +1,53 @@
+package pcd.ass02.dependencyAnalyserLib;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import java.io.File;
+
+public class DependencyAnalyserVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Promise<Void> startPromise) {
+        String projectPath = System.getProperty("user.dir") +
+            File.separator + "src" +
+            File.separator + "test" +
+            File.separator + "resources" +
+            File.separator + "test-project" +
+            File.separator;
+        String packagePath = projectPath + "p2" + File.separator;
+        String classSrcFile = packagePath + "B.java";
+
+        DependencyAnalyserLib.getClassDependencies(classSrcFile, this.vertx)
+            .onSuccess(classReport ->
+                System.out.println("Class report: " + classReport.toString()))
+            .onFailure(error ->
+                System.err.println("Error in class dependencies: " + error));
+
+        DependencyAnalyserLib.getPackageDependencies(packagePath, this.vertx)
+            .onSuccess(packageReport ->
+                System.out.println("Package report: " + packageReport.toString()))
+            .onFailure(error ->
+                System.err.println("Error in package dependencies: " + error));
+
+        DependencyAnalyserLib.getProjectDependencies(projectPath, this.vertx)
+            .onSuccess(projectReport ->
+                System.out.println("Project report: " + projectReport.toString()))
+            .onFailure(error ->
+                System.err.println("Error in project dependencies: " + error))
+            .onComplete(v -> {
+                vertx.close(ar -> {
+                    if (ar.succeeded()) {
+                        System.out.println("Vert.x closed gracefully.");
+                    } else {
+                        System.err.println("Error during Vert.x shutdown: " + ar.cause());
+                    }
+                });
+            });
+        startPromise.complete();
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("DependencyAnalyserVerticle stopped.");
+    }
+}
