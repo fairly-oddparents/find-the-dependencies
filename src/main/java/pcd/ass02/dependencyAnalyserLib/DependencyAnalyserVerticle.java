9a3c7a193d18bcaa08e3480e8b7a385b2b9b23f6
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2025-05-06 16:10:39 +0200

    refactor: improve output formatting for dependency reports

--- src/main/java/pcd/ass02/dependencyAnalyserLib/DependencyAnalyserVerticle.java
+++ src/main/java/pcd/ass02/dependencyAnalyserLib/DependencyAnalyserVerticle.java
@@ -19,19 +19,19 @@ public class DependencyAnalyserVerticle extends AbstractVerticle {
 
         DependencyAnalyserLib.getClassDependencies(classSrcFile, this.vertx)
             .onSuccess(classReport ->
-                System.out.println("[CLASS REPORT]\n: " + classReport.toString()))
+                System.out.println("[CLASS REPORT]:\n" + classReport.toString()))
             .onFailure(error ->
                 System.err.println("Error in class dependencies: " + error));
 
         DependencyAnalyserLib.getPackageDependencies(packagePath, this.vertx)
             .onSuccess(packageReport ->
-                System.out.println("[PACKAGE REPORT]\n: " + packageReport.toString()))
+                System.out.println("[PACKAGE REPORT]:\n" + packageReport.toString()))
             .onFailure(error ->
                 System.err.println("Error in package dependencies: " + error));
 
         DependencyAnalyserLib.getProjectDependencies(projectPath, this.vertx)
             .onSuccess(projectReport ->
-                System.out.println("[PROJECT REPORT]\n: " + projectReport.toString()))
+                System.out.println("[PROJECT REPORT]:\n " + projectReport.toString()))
             .onFailure(error ->
                 System.err.println("Error in project dependencies: " + error))
             .onComplete(v -> vertx.close());
